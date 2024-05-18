e5757066ae7016f4abc6339952653f24511ba0b7
KIlian42 <57774167+KIlian42@users.noreply.github.com>
2024-05-18 20:37:12 +0200

    Update domain description

--- src/vehicles10.lisp
+++ src/vehicles10.lisp
@@ -1,3 +1,11 @@
 	(truck-class c1 25 250 0.15 5 10 15 ())
-	(truck c1 truck1 0.0 4367 250 0 0)
-
+	(truck c1 truck1 0.0 g1 250 0 0)
+	(truck c1 truck2 0.0 g1 250 0 0)
+	(truck c1 truck3 0.0 g1 250 0 0)
+	(truck c1 truck4 0.0 g1 250 0 0)
+	(truck c1 truck5 0.0 g1 250 0 0)
+	(truck c1 truck6 0.0 g1 250 0 0)
+	(truck c1 truck7 0.0 g1 250 0 0)
+	(truck c1 truck8 0.0 g1 250 0 0)
+	(truck c1 truck9 0.0 g1 250 0 0)
+	(truck c1 truck10 0.0 g1 250 0 0)
